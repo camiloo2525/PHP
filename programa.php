fedd8a3d91eb585b1c436ed3610e3a74409f6422
juancamilo <juan.3802010536@ucaldas.edu.co>
2023-11-09 17:17:14 -0500

    nuevo cambio del programa.php

--- programa.php
+++ programa.php
@@ -15,7 +15,7 @@ echo " Son $horas horas, $minutos minutos y $segundos segundos \n";
 ?>
 
 <?php
-
+/*
 $horas = readline(" ingresa el numero de horas: ");
 $minutos = readline(" ingresa el numero de minutos: ");
 $segundos = readline(" ingresa el numero de segundos: ");
@@ -23,5 +23,16 @@ $segundos = readline(" ingresa el numero de segundos: ");
 $tiempo = ($horas * 60 *60) + ($minutos * 60) + $segundos;
 
 echo "Este tiempo equivale a $tiempo segundos " . "\n" ;
+*/
+?>
+
+<?php
+
+$hora = readline (" ingresar el numero de horas ");
+$minutos = readline ( " ingresar el numero de minutos ");
+$segundo = readline (" ingresar el numero de segundos ");
+
+$calculo = ($horas *3600) + ($minutos*60) + $segundos;
+echo "Los segundos calculados fueron :" . $calculo . "\n" ;
 
 ?>
